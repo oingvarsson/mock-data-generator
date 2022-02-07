785da69ff5a8ed7e17a4b73934ebda6b89b14e47
Ola Ingvarsson <ola.ingvarsson@gmail.com>
2022-02-07 15:21:56 +0100

    Cities

--- src/address/address.test.js
+++ src/address/address.test.js
@@ -1,8 +1,20 @@
-const address = require(".")
+const { streetAddress, zipCode, city } = require(".");
+const { cities } = require("./data");
 
-test('should be valid address', () => {
-  const a = address();
+test('should be valid street address', () => {
+  const a = streetAddress();
   const parts = a.split(' ');
   expect(typeof parts[0]).toBe('string');
   expect(Number.isInteger(parseInt(parts[parts.length-1]))).toBeTruthy();
 });
+
+test('should be five digit zip code', () => {
+  const z = zipCode();
+  expect(/\d{3}\s\d{2}/.test(z)).toBeTruthy();
+  expect(z).toHaveLength(6);
+});
+
+test('should be a city', () => {
+  const c = city();
+  expect(cities.includes(c)).toBeTruthy();
+})
--- src/address/index.js
+++ src/address/index.js
@@ -1,7 +1,16 @@
+const numberGroup = require("../common/number-group");
 const randomFromArray = require("../common/random-from-array");
 const number = require("../number");
-const { streets } = require("./data");
+const { streets, cities } = require("./data");
 
-const address = () => `${randomFromArray(streets)} ${number(1, 102)}`;
+const city = () => randomFromArray(cities);
 
-module.exports = address;
+const streetAddress = () => `${randomFromArray(streets)} ${number(1, 102)}`;
+
+const zipCode = () => `${numberGroup(3)} ${numberGroup(2)}`;
+
+module.exports = {
+  city,
+  streetAddress,
+  zipCode
+};
