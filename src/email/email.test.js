766aa5ef78a0a9e0667f8b47e885f005d939a928
Ola Ingvarsson <ola.ingvarsson@gmail.com>
2022-02-07 14:02:08 +0100

    More email tests

--- src/email/email.test.js
+++ src/email/email.test.js
@@ -2,4 +2,14 @@ const email = require(".")
 
 test('should have @ sign', () => {
   expect(email().includes('@')).toBeTruthy();
-})
+});
+
+test('should have custom prefix', () => {
+  const e = email({ prefix: 'tester' });
+  expect(e.startsWith('tester@')).toBeTruthy();
+});
+
+test('should have custom domain', () => {
+  const e = email({ domain: 'test.se' });
+  expect(e.endsWith('@test.se')).toBeTruthy();
+});
