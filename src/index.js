cbcc637c6c994b173368fad4e4ce86c46511eb43
Ola Ingvarsson <ola.ingvarsson@gmail.com>
2022-02-07 15:45:24 +0100

    Integration tests

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const address = require('./address');
+const boolean = require('./boolean');
+const email = require('./email');
+const name = require('./name');
+const number = require('./number');
+const phone = require('./phone');
+
+module.exports = {
+  address,
+  boolean,
+  email,
+  name,
+  number,
+  phone
+};
---
+++ src/index.test.js
@@ -0,0 +1,35 @@
+const { address, boolean, email, name, number, phone } = require(".")
+
+test('address integration', () => {
+  expect(address).toHaveProperty('streetAddress');
+  expect(address).toHaveProperty('city');
+  expect(address).toHaveProperty('zipCode');
+  expect(typeof address.streetAddress).toBe('function');
+  expect(typeof address.city).toBe('function');
+  expect(typeof address.zipCode).toBe('function');
+});
+
+test('boolean integration', () => {
+  expect(typeof boolean).toBe('function');
+});
+
+test('email integration', () => {
+  expect(typeof email).toBe('function');
+});
+
+test('name integration', () => {
+  expect(name).toHaveProperty('name');
+  expect(name).toHaveProperty('firstName');
+  expect(name).toHaveProperty('lastName');
+  expect(typeof name.name).toBe('function');
+  expect(typeof name.firstName).toBe('function');
+  expect(typeof name.lastName).toBe('function');
+});
+
+test('number integration', () => {
+  expect(typeof number).toBe('function');
+});
+
+test('phone integration', () => {
+  expect(typeof phone).toBe('function');
+});
