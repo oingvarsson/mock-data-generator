ea81eb715f9ccb1a1721f7280f51f8ec2f2d6270
Ola Ingvarsson <ola.ingvarsson@gmail.com>
2022-02-08 09:32:59 +0100

    Cleanup

--- src/email/index.js
+++ src/email/index.js
@@ -4,16 +4,16 @@ const { domains } = require('./data');
 
 /**
  * A random lower case email address
- * @param {Object} custom - Your prefered email details
- * @param {string} [custom.prefix=undefined] Customize the left side of the @-sign
- * @param {string} [custom.domain=undefined] Customize the right side of the @-sign
+ * @param {Object} options - Your prefered email details
+ * @param {string} [options.prefix=undefined] Customize the left side of the @-sign
+ * @param {string} [options.domain=undefined] Customize the right side of the @-sign
  * @returns {string}
  */
-const email = ( args={} ) => `${args.prefix || `${firstName()}.${lastName()}`}@${args.domain || randomFromArray(domains)}`
-.toLowerCase()
-.replace(/[åä]/g, 'a')
-.replace(/ö/g, 'o')
-.replace(/é/g, 'e')
-.replace(/[^a-z0-9.@]/g, '_');
+const email = ( options={} ) => `${options.prefix || `${firstName()}.${lastName()}`}@${options.domain || randomFromArray(domains)}`
+  .toLowerCase()
+  .replace(/[åä]/g, 'a')
+  .replace(/ö/g, 'o')
+  .replace(/é/g, 'e')
+  .replace(/[^a-z0-9.@]/g, '_');
 
 module.exports = email;
