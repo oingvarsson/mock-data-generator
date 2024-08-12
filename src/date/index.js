071f2cee632fdcd5260ac2c9a9a0929130f449a2
oingvarsson <ola.ingvarsson@gmail.com>
2024-08-12 11:25:26 +0200

    chore: Refactor randomDate function to set hours, minutes, and seconds

--- src/date/index.js
+++ src/date/index.js
@@ -1,6 +1,4 @@
 /** @module date */
-const numberGroup = require('../common/number-group');
-const randomFromArray = require('../common/random-from-array');
 const number = require('../number');
 
 const dateToMilliseconds = (date) => {
@@ -19,8 +17,12 @@ const dateToMilliseconds = (date) => {
  * @returns {Date}
  */
 const randomDate = (start = 0, end = Date.now()) => {
-  const d = number(dateToMilliseconds(start), dateToMilliseconds(end));
-  return new Date(d);
+  const d = new Date(
+    number(dateToMilliseconds(start), dateToMilliseconds(end))
+  );
+
+  d.setHours(number(0, 23), number(0, 59), number(0, 59), 0);
+  return d;
 };
 
 module.exports = randomDate;
